556c964d61b4b2881d2a7c53a79a796f790fa08d
caiman48 <caiman48clm@gmail.com>
2024-05-01 10:10:17 -0400

    small change

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -37,7 +37,7 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   try {
     const tagData = await Tag.create(req.body);
     res.status(201).json(tagData);
@@ -50,7 +50,7 @@ router.post('/', (req, res) => {
 router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
   try {
-    const tagData = await Tag.update(req.body, {
+    const [updated] = await Tag.update(req.body, {
       where: {
         id: req.params.id,
       },
